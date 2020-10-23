f9fe65b67b54bc134a897bd39253d5b72e3f4143
Daniel TIllero <danieltillero67@gmail.com>
2020-10-23 19:03:33 -0400

    parks page

--- routes.js
+++ routes.js
@@ -1,11 +1,27 @@
 const express = require("express");
 const { environment } = require("./config");
+const db = require("./db/models");
 
 const router = express.Router();
 
 router.get("/", (req, res) => {
     res.render("index", { title: "Home" });
 });
+
+const asyncHandler = (handler) => (req, res, next) =>
+    handler(req, res, next).catch(next);
+
+router.get(
+    "/parks",
+    asyncHandler(async (req, res) => {
+        const parks = await db.Park.findAll({
+            order: [["parkName", "ASC"]],
+        });
+
+        res.render("park-list", { title: "Parks", parks });
+        console.log("this got here");
+    })
+);
 
 if (environment !== "production") {
     router.get("/error-test", () => {
